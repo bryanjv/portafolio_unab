1eaa04ec6ace729bd8a07c0c78048ee16d089347
bryanjv <bryan.jeldes@gmail.com>
2023-01-12 14:01:28 -0300

    inicio de proyecto

---
+++ js/inicio.js
@@ -0,0 +1,4 @@
+var referencias = document.querySelector('head')
+referencias.innerHTML += 
+'<link rel="stylesheet" href="../css/style.css">' +
+'<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">';
