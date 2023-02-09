d83862360a51ed5ad900377b42e55e109852f452
bryanjv <bryan.jeldes@gmail.com>
2023-02-09 13:53:30 -0300

    Codigo base

--- js/inicio.js
+++
@@ -1,9 +0,0 @@
-var referencias = document.querySelector('head')
-referencias.innerHTML += 
-'<link rel="stylesheet" href="../css/style.css">' +
-'<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">';
-
-var footfoot = document.querySelector('footer');
-footfoot.innerHTML =
-'<div class="foot">' +
-'<h4>ESTE ES UN FOOTER MUY BONITO</h4>';
