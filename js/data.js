0177896d85258b60087978e9937cc45fb3abbb3f
bryanjv <bryan.jeldes@gmail.com>
2023-02-09 12:50:19 -0300

    cambio de visual proyecto

--- js/data.js
+++ js/data.js
@@ -1,22 +1,17 @@
-function savedata(data){
+function savedata(){
 
-    if (typeof window.localStorage.getItem(objrestaurant)){
+    var rest = [];
+    var nombre = document.querySelector('#input-nombre').value;
+    var info = document.querySelector('#input-menu').value;
 
+    if (window.localStorage.getItem('objrestaurant') != null){
+        rest = JSON.parse(window.localStorage.getItem('objrestaurant'));
+        console.log(rest);
+        rest.push({nombre: nombre, menu: info}); 
+        console.log(rest);
+        window.localStorage.setItem('objrestaurant', JSON.stringify(rest));
     }else{
-        var objrestaurant = {};
-        window.localStorage.setItem(objrestaurant,)
+        window.localStorage.setItem('objrestaurant', JSON.stringify([{nombre: nombre, menu: info}]));
     }
+    console.log(window.localStorage.getItem('objrestaurant'));
 }
-
-
-const objrestaurant = {
-    Chimichurri: window.localStorage.getItem("Chimichurri"),
-    Mcdolan: window.localStorage.getItem("Mcdolan"),
-    Vegetalking: window.localStorage.getItem("Vegetalking") 
-};
-
-function savedata(data){
-    window.localStorage.setItem(data.nombre, data.datos);
-}
-
-function getdata(data.nombre)
--- js/login.js
+++ js/login.js
@@ -4,12 +4,9 @@ function validar(){
 
     if (username == "admin" && password == "1234" ){
         window.location.href = 'restaurant.html';
-        // console.log(window.location);
-        // window.open("restaurant.html", "_self", true);
-        // console.log(window.location);
+
     }else{
         alert("Pone bien las credenciales >:C");
-        console.log('holi');
         return false;
     }
 }
