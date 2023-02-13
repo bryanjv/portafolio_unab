c3a9c2e5493538af72bd01a1f12dcfa8642d00a0
bryanjv <bryan.jeldes@gmail.com>
2023-02-13 12:48:07 -0300

    pendiente agregar node.js

--- js/AgregarCliente.js
+++ js/AgregarCliente.js
@@ -1,5 +1,28 @@
-import fs from 'fs';
+function AgregarCliente(formulario) {
 
-function AgregarCliente(formulario){
+    const fs = require('fs');
+  // Storing the JSON format data in myObject
+    var data = fs.readFileSync("../dataCliente.json");
+    var myObject = JSON.parse(data);
 
+    // Defining new data to be added
+
+    let nombre = formulario.nombreCliente.value;
+    let correo = formulario.correoCliente.value;
+    let direccion = formulario.direccionCliente.value;
+    let nacimiento = formulario.nacimientoCliente.value;
+
+
+    let newData = {nombre, correo, direccion, nacimiento};
+
+    // Adding the new data to our object
+    myObject.push(newData);
+
+    // Writing to our JSON file
+    var newData2 = JSON.stringify(myObject);
+    fs.writeFile("../dataCliente.json", newData2, (err) => {
+      // Error checking
+        if (err) throw err;
+            console.log("New data added");
+    });
 }
